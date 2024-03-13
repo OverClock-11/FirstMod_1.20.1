4ddf0d6ca48deecac6f6bca68b66405a0671f6d4
OverClock-11 <over.clock1101@gmail.com>
2024-03-12 21:13:57 -0700

    ItemCreated: Raw Sapphire Block

--- src/main/java/net/junhe/tutorialmod/item/ModCreativeModTabs.java
+++ src/main/java/net/junhe/tutorialmod/item/ModCreativeModTabs.java
@@ -25,6 +25,7 @@ public class ModCreativeModTabs {
                         output.accept(ModItems.RAW_SAPPHIRE.get());
                         output.accept(Items.DIAMOND);
                         output.accept(ModBlocks.SAPPHIRE_BLOCK.get());
+                        output.accept(ModBlocks.RAW_SAPPHIRE_BLOCK.get());
                     })
                     .build());
 
